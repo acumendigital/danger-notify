43f10a2f35ead75d37b21ac76a881cbb41e884b5
David Emaye <62448034+Emodot@users.noreply.github.com>
2021-06-28 15:28:16 +0100

    created nuxt-app

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'household-executives-admin',
+    title: 'danger-notify',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
